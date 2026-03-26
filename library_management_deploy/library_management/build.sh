92bd5aee6762758ab6ecbb716c51c49fd798f10e
TEJASREE <harishkumar36566@gmail.com>
2026-03-26 15:47:59 +0530

    Delete library_management_deploy directory

--- library_management_deploy/library_management/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Build script for Railway deployment
-pip install -r requirements.txt
-python manage.py collectstatic --no-input --settings=library_management.settings_prod
-python manage.py migrate --settings=library_management.settings_prod
